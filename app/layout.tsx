bb9074ef85849f4550c70edebcaab9419f915b17
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-05-07 20:05:29 +0800

    ✨ feat: add footer to test CD changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Fraunces } from 'next/font/google';
+import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 import './globals.css';
 
@@ -38,6 +39,7 @@ export default function RootLayout({
       <body className="flex flex-col min-block-full">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
